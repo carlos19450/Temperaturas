33d5f7fb2007978e72c1f31c4d56f4447aade9e0
carpui <carpui@ubuntu>
2021-12-13 17:52:46 +0100

    Muestro la temperatura mayor y menor de "n" municipios.

--- prueba1/src/prueba1/Prueba1.java
+++ prueba1/src/prueba1/Prueba1.java
@@ -19,35 +19,32 @@ public class Prueba1 {
         // TODO code application logic here
         String mun, basura;
         int cant;
-        double temp1 = 0, temp2 = 0, temp, tempactual;
+        double menor = 10000, mayor = -10000, temp, tempactual;
         Scanner read = new Scanner(System.in);
-        System.out.print("Introduce la temperatura actual donde esta usted: ");
-        tempactual = read.nextDouble();
+
         System.out.println("¿Cuantos municipios quiere poner para saber su temperatura?");
         cant = read.nextInt();
+
         //Bucle
         for (int i = 1; i <= cant; i++) {
-            System.out.print("Escribe un municipio: ");
-            mun = read.nextLine();
-            basura = read.nextLine();
-            System.out.print("Dime su temperatura: ");
+            //System.out.print("Escribe un municipio: ");
+            //mun = read.nextLine();
+            //basura = read.nextLine();
+            System.out.print("Dime la temperatura: ");
             temp = read.nextDouble();
             if (temp < 0) {
-                System.out.println("La temperatura de " + mun + " es negativa.");
+                System.out.println("La temperatura de " + temp + " es negativa.\n");
             } else {
-                System.out.println("La temperatura de " + mun + " es positiva.");
+                System.out.println("La temperatura de " + temp + " es positiva.\n");
             }
-            if (temp < tempactual) {
-                temp1 = temp;
-            } else if (temp > tempactual) {
-                temp2 = temp;
+            if (temp < menor) {
+                menor = temp;
+            }
+            if (temp > mayor) {
+                mayor = temp;
             }
         }
-        if (temp1 < temp2) {
-            System.out.println("La temperatura maxima es: " + temp2);
-        } else if (temp1 > temp2) {
-            System.out.println("La temperatura minima es: " + temp1);
-        }
+        System.out.println("La temperatura menor es: "+menor+"\n La temperatuera mayor es: "+mayor);
     }
 
 }
