cfac5e70d0920a5f1b703d5aa7e16cb65cea4f9f
carpui <carpui@ubuntu>
2021-12-20 17:24:27 +0100

    Tempreraturas y municipios añadidos al final del programa con arrays.

--- TemperaturasCarlos/src/temperaturascarlos/TemperaturasCarlos.java
+++ TemperaturasCarlos/src/temperaturascarlos/TemperaturasCarlos.java
@@ -14,23 +14,22 @@ public class TemperaturasCarlos {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        String mun;
         int cant;
         double menor = Double.MAX_VALUE, mayor = Double.MIN_VALUE, temp = 0;
         Scanner read = new Scanner(System.in);
 
         System.out.println("¿Cuantos municipios quiere poner para saber su temperatura?");
         cant = read.nextInt();
+        String[] mun = new String[cant]; 
         double[] num = new double[cant];
-        System.out.println("Dime el nombre de los municipios: ");
-        mun = read.next();
-        String[] num2 = new String[cant];
+        
         //Bucles 1 array
         for (int i = 0; i < num.length; i++) {
+            System.out.println("Dime el nombre del municipio:");
+            mun[i] = read.next();
             System.out.print("Dime la temperatura: ");
             num[i] = read.nextDouble();
             temp = num[i];
-            //temp = read.nextDouble();
             if (temp < 0) {
                 System.out.println("La temperatura de " + temp + " es negativa.\n");
             } else {
@@ -43,16 +42,18 @@ public class TemperaturasCarlos {
                 mayor = temp;
             }
         }
+        
         //Bucles 2 array
         double acum;
-        System.out.println("Las temperaturas son:\n");
+        System.out.println("");
         for (int i = 0; i < num.length; i++) {
             acum = num[i];
-            System.out.println(acum);
+            System.out.println("Las temperaturas de "+mun[i]+" es: "+acum+"\n");
         }
         System.out.println("");
+        
         //Temperatura mayor y menor
-        System.out.println("La temperatura menor es: " + menor);
+        System.out.println("El municipio  de la temperatura mayor es " + menor);
         System.out.println("La temperatuera mayor es: " + mayor);
     }
 
